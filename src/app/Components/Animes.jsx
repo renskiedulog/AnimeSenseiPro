924b7ba3c5b8af177ac3161d6b5159706fd139d6
renskiedulog <22104607@gmail.com>
2024-02-03 16:31:00 +0800

    Fix: Anime Image Size

--- src/app/Components/Animes.jsx
+++ src/app/Components/Animes.jsx
@@ -92,7 +92,7 @@ export const Animes = ({ filter, search, recents }) => {
                   className="group"
                 >
                   <img
-                    className="z-10 mx-auto md:aspect-[1/1.5] aspect-[1/1.2] w-full rounded object-cover"
+                    className="z-10 mx-auto md:aspect-[1/1.3] w-full rounded object-cover"
                     src={anime?.image}
                     alt={anime?.title}
                   />
